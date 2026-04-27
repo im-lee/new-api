4b37a4cb5fda05261a0be74bba8343037fab5e51
lir <1457781180@qq.com>
2026-04-27 18:07:30 +0800

    fix:修复首页白屏无内容显示问题

--- web/classic/src/components/layout/headerbar/index.jsx
+++ web/classic/src/components/layout/headerbar/index.jsx
@@ -105,6 +105,7 @@ const HeaderBar = ({ onMobileMenuToggle, drawerOpen }) => {
             isLoading={isLoading}
             userState={userState}
             pricingRequireAuth={pricingRequireAuth}
+            t={t}
           />
 
           <ActionButtons
